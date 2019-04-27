b2f7743e625dd75a65cdc500e11efa576e6230a8
AlvaradoJesus <jesusmas1202@gmail.com>
2019-04-26 21:40:07 -0700

    First commit
    Add app with materialize, font awesome and vue js http post example

---
+++ HackatonProject/HackatonProject/app/app.js
@@ -0,0 +1,29 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!',
+        mainUrl: ''
+    },
+    created: function () {
+        $(document).ready(function () {
+            $('.sidenav').sidenav();
+        });
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        },
+        hola: function () {
+            axios
+                .get('/home/hola')
+                .then(response => {
+                    this.info = response.data.bpi
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        }
+    }
+})
