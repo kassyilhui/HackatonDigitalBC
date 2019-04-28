b7e749d47e77837fe674e40d7249f9d190a6afb6
AlvaradoJesus <jesusmas1202@gmail.com>
2019-04-28 00:38:42 -0700

    Agregar autocomplete google map
    Campo para buscar lugares

--- HackatonProject/HackatonProject/app/app.js
+++ HackatonProject/HackatonProject/app/app.js
@@ -2,16 +2,23 @@
     load: {
         key: 'AIzaSyB0ualSPp-6O-QxrnHyc4CwzMPIufKzRmQ',
         v: '3.26',
+        libraries: 'places'
     },
     // Demonstrating how we can customize the name of the components
-    installComponents: 'places',
+    installComponents: true
+
 });
 
 var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!',
-        mainUrl: ''
+        mainUrl: '',
+        center: {
+            lat: 10.0,
+            lng: 10.0
+        },
+        currentPlace: {}
     },
     created: function () {
         $(document).ready(function () {
@@ -34,6 +41,20 @@ var app = new Vue({
                     this.errored = true
                 })
                 .finally(() => this.loading = false)
+        },
+        setPlace(place) {
+            this.currentPlace = place;
+        },
+        addMarker() {
+            if (this.currentPlace) {
+                this.center = {
+                    lat: this.currentPlace.geometry.location.lat(),
+                    lng: this.currentPlace.geometry.location.lng(),
+                };
+                
+                this.currentPlace = null;
+            }
         }
+        
     }
 })
